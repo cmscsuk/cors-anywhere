aaf5279bb03b16a09547d2742fa0d35c889b4e1d
cmscs <75048622+cmscs@users.noreply.github.com>
2021-04-23 23:17:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
